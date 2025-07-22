e8f79b3b1097b0130a90521bcc0659d184288a06
Tjaden Hess <tjade273@gmail.com>
2025-07-22 10:54:31 -0400

    Start client implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod transparency {
+    tonic::include_proto!("transparency");
+}
+mod kt {
+    tonic::include_proto!("kt");
+}
+
+use kt::key_transparency_service_client::KeyTransparencyServiceClient;
+
 fn main() {
-    println!("Hello, world!");
+    let mut client = KeyTransparencyServiceClient::
+
+    let request = tonic::Request::new(kt::AuditRequest {
+        start: 0,
+        limit: 100,
+    });
+
+    let response = client.audit(request).unwrap();
 }
