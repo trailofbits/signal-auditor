50f935b143bcfc1144d2738cf6be1113495d4eb5
Tjaden Hess <tjade273@gmail.com>
2025-07-31 16:26:20 -0400

    fix benches

--- benches/audit.rs
+++ benches/audit.rs
@@ -1,11 +1,13 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
 use ed25519_dalek::SigningKey;
-use rand::{Rng, TryRngCore, rngs::OsRng};
+use rand::{TryRngCore, rngs::OsRng};
 use signal_auditor::{
     auditor::{Auditor, DeploymentMode, PublicConfig},
     transparency::TransparencyLog,
 };
+use std::hint::black_box;
 use std::fs;
+use signal_auditor::proto::transparency;
 
 mod test_vectors {
     include!(concat!(env!("OUT_DIR"), "/test_vectors.rs"));
